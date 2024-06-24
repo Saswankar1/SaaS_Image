d84ab532661a423f90ce256424b29db2f741b1da
Saswankar1 <dragodgames45@gmail.com>
2024-06-24 14:22:22 +0530

    Initial commit

---
+++ Backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+// GET -> get some data from db
+// PUT -> modify data 
+// POST -> send data
+// DELETE -> delete some data
+
+// Middleware->funct which handle requests, next goes to next middleware, res send response
+// Dynamic Routing
+app.delete("/user/:id", (req, res, next) => {
+  console.log(req.params.id);
+  return res.send("hello");
+});
+
+
+// tells apps the 
+app.use(express.json());
+
+app.listen(5001, ()=>console.log('listening on port'));
